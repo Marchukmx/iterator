ce02cbf97eb3c5380e48d1a040113af83896fe5e
marchukmx <mmmarch1k@gmail.com>
2023-05-18 19:58:15 +0300

    #1

--- main.py
+++ main.py
@@ -1,49 +1,29 @@
-# ітератор
-lst = [1,2,3,4,5,6]
-print(iter(lst))
+#1
 
-class MyIterator:
-    def __init__(self,data):
-        self.data = data
-        self.index = 0
+class OddIterator:
+    def __init__(self, n):
+        if n < 1:
+            raise ValueError("Неправильне значення N")
+        self.n = n
+        self.nepar = 1
+
     def __iter__(self):
         return self
+
     def __next__(self):
-        if self.index >= len(self.data):
-            raise StopIteration
-        value = self.data[self.index]
-        self.index += 1
-        return value
-
-my_list = [ 1,2,3,4,5]
-my_iter = MyIterator(my_list)
-for num in my_iter:
-    print(num)
-
-#генератор
-
-def my_generator(data):
-    for item in data:
-        yield item
-
-for num in my_generator(my_list):
-    print(num)
+        if self.nepar > self.n:
+            raise StopIteration("Кінець ітерації")
+        odd_number = self.nepar
+        self.nepar += 2
+        return odd_number
+try:
+    n = int(input("Введіть значення N: "))
+    iterator = OddIterator(n)
+    for number in iterator:
+        print(number)
+except ValueError as e:
+    print("Помилка:", e)
+except StopIteration as e:
+    print("Кінець ітерації:", e)
 
 
-#КАЛЬКУЛЯТОР
-
-def calculator():
-    def add(a,b):
-        return a + b
-    def sub(a,b):
-        return a - b
-    def mult(a,b):
-        return a * b
-    def div(a,b):
-        if b != 0:
-            return a / b
-        else:
-            raise ValueError("ПОМИЛКА")
-    return add,sub,mult,div
-add,sub,mult,div = calculator()
-print(div(3,0))
