711f91deea8cc2fb8ba9735e14c193d0267698c3
marchukmx <mmmarch1k@gmail.com>
2023-05-18 20:31:12 +0300

    #4

--- main.py
+++ main.py
@@ -1,27 +1,11 @@
-#1
+#4
 
-class OddIterator:
-    def __init__(self, n):
-        if n < 1:
-            raise ValueError("Неправильне значення N")
-        self.n = n
-        self.current = 1
+def multiply_by(n):
+    def zamik(x):
+        return x * n
+    return zamik
 
-    def __iter__(self):
-        return self
+multiply_by_n = multiply_by(n = int(input("Введіть число:")))
+result = multiply_by_n(int(input("Введіть кількість замиканнь:")))
+print(result)
 
-    def __next__(self):
-        if self.current > self.n:
-            raise StopIteration("Кінець ітерації")
-        odd_number = self.current
-        self.current += 2
-        return odd_number
-try:
-    n = int(input("Введіть значення N: "))
-    iterator = OddIterator(n)
-    for number in iterator:
-        print(number)
-except ValueError as e:
-    print("Помилка:", e)
-except StopIteration as e:
-    print("Кінець ітерації:", e)
